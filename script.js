7e2341da1f515dbc113f2002e7f3c657a60906b4
ticyoliveiraticy-cpu <ticyoliveiraticy@gmail.com>
2026-01-22 14:01:41 -0300

    Adicionado função para o botão

---
+++ script.js
@@ -0,0 +1,8 @@
+// Obtém o botão com id "myButton"
+const button = document.getElementById('myButton');
+
+// Adiciona um evento de clique ao botão
+button.addEventListener('click', function() {
+    // Redireciona o usuário para o site do YouTube
+    window.location.href = 'https://www.youtube.com/@horadoqa';
+});
